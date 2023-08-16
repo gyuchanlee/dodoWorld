42b450956cb81696a60979a9d28259c1c2ae1be5
gyuchanlee <hoo788@naver.com>
2023-08-16 15:47:37 +0900

    2023-08-16 first commit

---
+++ src/main/java/com/dodo/dodoworld/DodoWorldApplication.java
@@ -0,0 +1,13 @@
+package com.dodo.dodoworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodoWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dodo/dodoworld/DodoWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodo.dodoworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
