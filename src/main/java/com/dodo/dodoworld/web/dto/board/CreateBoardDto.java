47c75e682aec56dcb1e9076a29e6620477321f17
gyuchanlee <hoo788@naver.com>
2023-08-22 16:51:23 +0900

    2023-08-22 : 게시글 저장 부분 구현중 > layout 템플릿 깨짐현상, enum부분 라디오 적용, 검증 구현 필요.

--- src/main/java/com/dodo/dodoworld/web/dto/board/CreateBoardDto.java
+++ src/main/java/com/dodo/dodoworld/web/dto/board/CreateBoardDto.java
@@ -2,11 +2,17 @@ package com.dodo.dodoworld.web.dto.board;
 
 import com.dodo.dodoworld.domain.BoardCategories;
 import com.dodo.dodoworld.domain.Member;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class CreateBoardDto {
 
     private String writer; // 작성자 id값
     private BoardCategories boardCategories;
-
+    private String content;
     private String title;
 }
